297b76c2b441eb260979d80af3308eeee38456c9
BZ <435878393@qq.com>
2023-09-07 01:40:33 +0800

    增加Server酱通道

--- UTILS/channel/__init__.py
+++ UTILS/channel/__init__.py
@@ -1,5 +1,6 @@
 from ._factory import ChannelFactory
 
 from ._work_weixin_channel import WorkWeixinChannel
+from ._server_chan_channel import ServerChanChannel
 
 __all__ = ['ChannelFactory']
---
+++ UTILS/channel/_server_chan_channel.py
@@ -0,0 +1,40 @@
+import json
+from enum import Enum
+
+from ._factory import ChannelFactory
+from ._channel_base import Channel
+
+
+# https://sct.ftqq.com/
+
+class MsgType(Enum):
+    MARKDOWN = 'markdown'
+
+
+def _get_markdown_message(title: str, desp: str, *args, **kwargs):
+    return {
+        'title': title,
+        'desp': desp
+    }
+
+
+class ServerChanChannel(Channel):
+    def __init__(self):
+        super(ServerChanChannel, self).__init__()
+
+    def _get_webhook(self, send_key: str):
+        return f"https://sctapi.ftqq.com/{send_key}.send"
+
+    def get_post_args(self, send_key: str, msg_type: str, *args, **kwargs):
+        msg_type = MsgType(msg_type)
+        if msg_type == MsgType.MARKDOWN:
+            message = _get_markdown_message(*args, **kwargs)
+        else:
+            raise NotImplementedError
+
+        message_json = json.dumps(message)
+        webhook = self._get_webhook(send_key=send_key)
+        return {'url': webhook, 'data': message_json}
+
+
+ChannelFactory.register(channel_name='Server酱', channel_class=ServerChanChannel)
